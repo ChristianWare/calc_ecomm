cae59fb11c09e70ac29fe60fe1eeeb003fbcf14f
Chris Ware <c.p.ware@hotmail.com>
2024-02-04 21:44:29 -0700

    sending emails

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,6 +3,7 @@ import UserModel from "@/app/models/userModel";
 import { NewUserRequest } from "@/app/types";
 import { NextResponse } from "next/server";
 import { hash } from "bcrypt";
+import nodemailer from "nodemailer";
 
 export const POST = async (req: Request) => {
   const body = (await req.json()) as NewUserRequest;
@@ -11,6 +12,20 @@ export const POST = async (req: Request) => {
     ...body,
   });
 
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "fbd895ded3fc77",
+      pass: "10a5a3a32d9c23",
+    },
+  });
+
+  await transport.sendMail({
+    from: "verification@nextecom.com",
+    to: newUser.email,
+    html: `<h1>Please verify your email by clicking on <a href=http://localhost:3000">this link</a></h1>`,
+  });
 
   return NextResponse.json(newUser);
 };
