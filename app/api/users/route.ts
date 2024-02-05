483f5905dd20aac3d0410ba58b4849cb93a6a32a
Chris Ware <c.p.ware@hotmail.com>
2024-02-04 23:28:44 -0700

    verification token + page

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,22 @@
+import EmailVerificationToken from "@/app/models/emailVerificationToken";
+import { NewUserRequest } from "@/app/types";
 import startDb from "@/app/lib/db";
 import UserModel from "@/app/models/userModel";
-import { NewUserRequest } from "@/app/types";
 import { NextResponse } from "next/server";
-import { hash } from "bcrypt";
 import nodemailer from "nodemailer";
+import crypto from "crypto";
 
 export const POST = async (req: Request) => {
   const body = (await req.json()) as NewUserRequest;
   await startDb();
   const newUser = await UserModel.create({
     ...body,
+  });
+
+  const token = crypto.randomBytes(36).toString("hex");
+  await EmailVerificationToken.create({
+    user: newUser._id,
+    token,
   });
 
   const transport = nodemailer.createTransport({
@@ -21,10 +28,12 @@ export const POST = async (req: Request) => {
     },
   });
 
+  const verificationUrl = `http://localhost:3000/verify?token=${token}&userId=${newUser._id}`;
+
   await transport.sendMail({
     from: "verification@nextecom.com",
     to: newUser.email,
-    html: `<h1>Please verify your email by clicking on <a href=http://localhost:3000">this link</a></h1>`,
+    html: `<h1>Please verify your email by clicking on <a href=${verificationUrl}>this link</a></h1>`,
   });
 
   return NextResponse.json(newUser);
