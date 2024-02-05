11bb9b9877f6150c214d5d2110d903d50611160a
Chris Ware <c.p.ware@hotmail.com>
2024-02-05 14:25:51 -0700

    Added Next Auth

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,15 +1,11 @@
 import mongoose from "mongoose";
 
-// let connection: typeof mongoose;
-
-// const url = "mongodb: //127.0.0.1:27017/next_ecom";
-
 const startDb = async () => {
   try {
     await mongoose.connect(process.env.DB_URI!);
     console.log("MongoDB Connected");
   } catch (error) {
-    throw new Error((error as any).message);
+    throw new Error("Connection Failed");
   }
 };
 
