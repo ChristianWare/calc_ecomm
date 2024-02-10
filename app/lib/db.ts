915e3628dd4341f0cbe1c860957148c0dcc1ce4b
Chris Ware <c.p.ware@hotmail.com>
2024-02-09 17:31:41 -0700

    genereated token

---
+++ app/lib/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let connection: typeof mongoose;
+
+const url = process.env.MONGODB_URI!;
+
+const startDb = async () => {
+  try {
+    if (!connection) {
+      connection = await mongoose.connect(url);
+      console.log("Connected to MongoDB");
+    }
+    return connection;
+  } catch (error) {
+    throw new Error((error as any).message);
+  }
+};
+
+export default startDb;
