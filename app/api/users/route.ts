ff3ff60a0212f96403bb79e1b54b24b0c7afa280
Chris Ware <c.p.ware@hotmail.com>
2024-02-09 19:36:10 -0700

    API Route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,7 @@
-import startDb from "@/app/lib/db";
-import UserModel from "@/app/models/userModel";
-import { NewUserRequest } from "@/app/types";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
-  const body = (await req.json()) as NewUserRequest;
-  await startDb();
-
-  const newUser = await UserModel.create({
-    ...body,
-  });
-
-  return NextResponse.json(newUser);
+  const values = await req.json();
+  console.log(values);
+  return NextResponse.json({ ok: true, from: "From API Folder" });
 };
